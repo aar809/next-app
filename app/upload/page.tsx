2de1872d0afba6a61b18458c2b2ec6deb4eda006
Aaron Chan <a.chan809@gmail.com>
2025-02-24 03:01:47 +0800

    setup cloudinary upload file features

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import { CldUploadWidget } from 'next-cloudinary'
+'use client';
+import React, { useState } from 'react'
+import { CldUploadWidget, CldImage } from 'next-cloudinary'
+
+
+interface CloudinaryResult {
+    public_id: string;
+}
 
 const UploadPage = () => {
+    const [publicId, setPublicId] = useState('');
     return (
-        <CldUploadWidget uploadPreset=''>
-        </CldUploadWidget>
+        <>
+            {publicId && <CldImage src={publicId} width={270} height={180} alt="a coffee" />}
+            <CldUploadWidget
+                uploadPreset='abcdef'
+                options={{
+                    sources: ['local'],
+                    multiple: false,
+                    maxFiles: 5,
+                }}
+                onUpload={(result, widget) => {
+                    if (result.event !== 'success') return;
+                    const info = result.info as CloudinaryResult;
+                    setPublicId(info.public_id)
+                    console.log(result)
+                }}>
+                {({ open }) => <button
+                    className='btn btn-primary'
+                    onClick={() => open()}>Upload</button>}
+            </CldUploadWidget>
+        </>
     )
 }
 
